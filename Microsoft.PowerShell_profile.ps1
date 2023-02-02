1cafeeb8e6496e3677c847be64ba34d0d9a5b4ae
przemd <49760438+przemd@users.noreply.github.com>
2023-02-02 18:16:43 +0100

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,5 @@
 #modules
+#######################################
 if ((Get-Module Posh-Git -ErrorAction SilentlyContinue -ListAvailable) -eq $null)
 {
 	Install-Module Posh-Git -Force -scope currentUser
@@ -28,6 +29,7 @@ if ((Get-Module az -ErrorAction SilentlyContinue -ListAvailable) -eq $null)
 }
 
 #aliases
+################################################
 New-Alias npp -Value "C:\Program Files\Notepad++\notepad++.exe" -Force
 
 #functions
@@ -37,5 +39,33 @@ function Prompt {
 	}
 	
 #run at startup
+########################################################
 Clear-Host
 
+#other software
+###############################################
+# Check if Chocolatey is installed
+if (!(Get-Command choco -ErrorAction SilentlyContinue)) {
+  Write-Host "Chocolatey is not installed. Installing..."
+  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+# Check if kubectl is installed
+if (!(Get-Command kubectl -ErrorAction SilentlyContinue)) {
+  Write-Host "kubectl is not installed. Installing..."
+  choco install kubernetes-cli
+}
+# Check if the Azure CLI is installed
+if (!(Get-Command az -ErrorAction SilentlyContinue)) {
+  Write-Host "The Azure CLI is not installed. Installing..."
+  choco install azure-cli
+}
+# Check if Helm is installed
+if (!(Get-Command helm -ErrorAction SilentlyContinue)) {
+  Write-Host "Helm is not installed. Installing..."
+  choco install kubernetes-helm
+}
+# Check if K9s is installed
+if (!(Get-Command k9s -ErrorAction SilentlyContinue)) {
+  Write-Host "K9s is not installed. Installing..."
+  choco install k9s
+}
